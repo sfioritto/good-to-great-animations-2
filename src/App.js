46cd29a224842a74102ba7602edc36eb6d2143c6
Sean Fioritto <sean@planningforaliens.com>
2018-10-02 12:16:16 -0500

    loader loads a container.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 class Loader extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      progress: 0
+      progress: 0,
+      loaded: false
     };
   }
 
@@ -17,18 +19,30 @@ class Loader extends Component {
         progress: this.state.progress + 1
       });
       setTimeout(this.start.bind(this), 10);
+    } else {
+      this.setState({loaded: true});
     }
   }
 
+  unloaded() {
+    return this.props.unloaded({
+      progress: this.state.progress,
+      onClick: this.start.bind(this)
+    });
+  }
+
+  loaded() {
+    return this.props.loaded();
+  }
+
   render() {
-    return (
-      <div className="loader">
-        {this.props.render({
-          progress: this.state.progress,
-          onClick: this.start.bind(this)
-        })}
-      </div>
-    );
+    if (this.state.loaded) {
+      return this.loaded();
+    } else {
+      return (
+        <div className="loader">{this.unloaded()}</div>
+      );
+    }
   }
 }
 
@@ -63,20 +77,61 @@ function Button(props) {
   );
 }
 
+function Container(props) {
+  return (
+    <TransitionGroup
+      className="container">
+      {props.items.map(({key, child}) => {
+        return (
+          <CSSTransition
+            key={key}
+            {...props}>
+            {child}
+          </CSSTransition>
+        );
+      })}
+    </TransitionGroup>
+  );
+}
+
+function SimpleCard(props) {
+  return (
+    <div className="simple-card">
+      <div className="title"></div>
+      <div className="sub-title"></div>
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
       <div className="app-frame">
         <Loader
-          render={(props) => {
+          unloaded={(props) => {
             return (
               <Button
                 value="Load the App!"
                 {...props}
                 />
             );
-          }}/>
-
+          }}
+        loaded={(props) => {
+          const items = [1, 2, 3, 4, 5, 6].map(key=>{
+            return {
+              key: key,
+              child: (<SimpleCard />)
+            };
+          });
+          return (
+            <Container
+              items={items}
+              timeout={100}
+              classNames="simple-card"
+              appear
+              />
+          );
+        }}/>
       </div>
     );
   }
