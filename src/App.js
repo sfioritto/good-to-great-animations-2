a306c874d8079c13e3512868075a07ce2cd5c4e2
Sean Fioritto <sean@planningforaliens.com>
2018-10-02 10:40:21 -0500

    basic button.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Button(props) {
+
+  let innerJSX,
+      classNames = "progress-bar-loader";
+
+  if (props.progress === 0) {
+    innerJSX = <span className="overlay">{props.value}</span>;
+  } else if (props.progress === 100) {
+    classNames += " loaded";
+    innerJSX = "Success!";
+  } else {
+    classNames += " loading";
+    innerJSX = (
+      <span
+        className="progress-bar"
+        style = {{width: props.progress + "%"}}
+        >
+      </span>
+    );
+  }
+
+  return (
+    <button
+      className={classNames}
+      onClick={props.onClick}
+      >
+      {innerJSX}
+    </button>
+  );
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app-frame">
+      <Button
+        value="Load the App!"
+        progress={0}
+        onClick={() => console.log("click")}
+        />
       </div>
     );
   }
